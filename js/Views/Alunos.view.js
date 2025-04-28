a1abe3cf32878260fdfd44dc1c3b3ceb5017782a
mikek182 <marcosvinicius@geekwork.com.br>
2025-04-28 16:38:55 -0300

    Gerando o id do aluno

--- js/Views/Alunos.view.js
+++ js/Views/Alunos.view.js
@@ -1,5 +1,17 @@
 class AlunosView {
-    constructor() {
+    constructor({nome, _id, notas} = {notas: {}}) {
+        this.nome = nome;
+        this._id = (_id !== undefined)? _id : this.generateId();
         
+        if (this._id > AlunosModel.maxId) {
+            AlunosModel.maxId = this._id;
+        }
+
+    }
+
+    generateId() {
+        return AlunosModel.maxId + 1
     }
 }
+
+AlunosModel.maxId = 0
