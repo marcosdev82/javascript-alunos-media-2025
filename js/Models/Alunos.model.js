03c190db07f9a9ed230a93dfeb316d5e7165f0b1
mikek182 <marcosvinicius@geekwork.com.br>
2025-04-28 16:40:08 -0300

    Refatorando classes

--- js/Models/Alunos.model.js
+++ js/Models/Alunos.model.js
@@ -1,5 +1,16 @@
 class AlunosModel {
     constructor() {
-        
+        constructor({nome, _id, notas} = {notas: {}}) {
+            this.nome = nome;
+            this._id = (_id !== undefined)? _id : this.generateId();
+            
+            if (this._id > AlunosModel.maxId) {
+                AlunosModel.maxId = this._id;
+            }
+        }
+    
+        generateId() {
+            return AlunosModel.maxId + 1
+        }
     }
 }
--- js/Views/Alunos.view.js
+++ js/Views/Alunos.view.js
@@ -1,17 +1,5 @@
 class AlunosView {
-    constructor({nome, _id, notas} = {notas: {}}) {
-        this.nome = nome;
-        this._id = (_id !== undefined)? _id : this.generateId();
-        
-        if (this._id > AlunosModel.maxId) {
-            AlunosModel.maxId = this._id;
-        }
-
-    }
-
-    generateId() {
-        return AlunosModel.maxId + 1
-    }
+    
 }
 
 AlunosModel.maxId = 0
