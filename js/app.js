5e9e6feaa0877a96e1fa01a73d965a1233aa6a09
mikek182 <marcosvinicius@geekwork.com.br>
2025-04-28 12:58:11 -0300

    Formatação da tabela

--- js/app.js
+++ js/app.js
@@ -6,11 +6,22 @@ alunos.forEach(aluno => {
 });
 
 const htmlHeader = document.createElement('tr')
-htmlHeader.innerHTML = "<td>Nome</td>"
 
-const htmlHeaderMaterias = Object.keys(alunos[0].notas).map(materia => `<td>${materia}</td>`).join('')
+
+htmlHeader.innerHTML = `<td class="px-5 py-3 text-left">Nome</td>`
+
+const htmlHeaderMaterias = Object.keys(alunos[0].notas).map(materia => `<td class="px-5 py-3 text-left">${materia}</td>`).join('')
 
 htmlHeader.innerHTML += htmlHeaderMaterias
-// console.log(htmlHeaderMaterias)
 
 document.querySelector('[data-table="alunos"] thead').appendChild(htmlHeader)
+
+alunos.forEach(aluno => {
+    const htmlRow = document.createElement('tr')
+    htmlRow.className = 'odd:bg-white even:bg-gray-100';
+    htmlRow.innerHTML = `<td class="px-6 py-3 text-left">${aluno.nome}</td>`
+    // console.log(Object.keys(aluno.notas)) - atenção para o uso de Object.keys, que retorna um array com as chaves do objeto
+    const htmlRowMaterias = Object.keys(aluno.notas).map(materia => `<td>${aluno.media[materia]}</td>`).join('')
+    htmlRow.innerHTML += htmlRowMaterias
+    document.querySelector('[data-table="alunos"] tbody').appendChild(htmlRow)
+})
