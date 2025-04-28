af4cc60b0faaf674c40a1e03b0c6e471d0f468f8
mikek182 <marcosvinicius@geekwork.com.br>
2025-04-28 16:55:45 -0300

    inclusão dos script model e service

--- js/app.js
+++ js/app.js
@@ -1,8 +1,28 @@
+const alunos = [
+{
+    _id: 0,
+    nome: "chico melato",
+    notas: {
+    portugues: [1, 1, 2, 2],
+    matematica: [2, 2, 2, 2],
+    historia: [2, 2, 3, 3],
+    ciencias: [3, 3, 3, 3],
+    },
+},
+{
+    _id: 1,
+    nome: "talita lima",
+    notas: {
+    portugues: [4, 4, 4, 4],
+    matematica: [4, 4, 5, 5],
+    historia: [5, 5, 6, 6],
+    ciencias: [7, 7, 8, 9],
+    },
+},
+];
+
 alunos.forEach(aluno => {
-    aluno.media = {}
-    for(let materia in aluno.notas) {
-        aluno.media[materia] = avarege(...aluno.notas[materia])
-    }
+    
 });
 
 const htmlHeader = document.createElement('tr')
