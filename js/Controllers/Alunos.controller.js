79421ab10e2d4729bd1b1d998bc05cd2b5fac3fa
mikek182 <marcosvinicius@geekwork.com.br>
2025-04-30 15:15:55 -0300

    Campo edit nota

--- js/Controllers/Alunos.controller.js
+++ js/Controllers/Alunos.controller.js
@@ -1,13 +1,12 @@
 class AlunosController {
     constructor(service, view) {
-        view.render(service.alunos)
+        view.renderTbody(service.alunos)
         this.view = view
         this.service = service
     }
 
     add(aluno) {
-        this.view.tableBody.innerHTML = ''
         this.service.add(new AlunosModel(aluno))
-        this.view.render(this.service.alunos)   
+        this.view.renderTbody(this.service.alunos)   
     }
 }
--- js/Views/Alunos.view.js
+++ js/Views/Alunos.view.js
@@ -3,31 +3,70 @@ class AlunosView {
         this.tableList = table;
         this.tableHeader = this.tableList.querySelector('thead');
         this.tableBody = this.tableList.querySelector('tbody');
-        this.materias = ["portugues","matematica","historia","ciencias"];
-   
+        this.materias = ["portugues", "matematica", "historia", "ciencias"];
+        this.boxEdit = '';
+
         this.renderHeader();
     }
 
     renderHeader() {
-        const htmlHeader = document.createElement('tr')
-        htmlHeader.innerHTML = `<td class="px-5 py-3 text-left">Nome</td>`
+        const htmlHeader = document.createElement('tr');
+        htmlHeader.innerHTML = `<td class="px-5 py-3 text-left">Nome</td>`;
 
-        const htmlHeaderMaterias = this.materias.map(materia => `<td class="px-5 py-3 text-left">${materia}</td>`).join('')
+        const htmlHeaderMaterias = this.materias
+            .map(materia => `<td class="px-5 py-3 text-left">${materia}</td>`)
+            .join('');
 
-        htmlHeader.innerHTML += htmlHeaderMaterias
-        document.querySelector('[data-table="alunos"] thead').appendChild(htmlHeader)
-    }    
-
-    render(alunos) {
-
-        alunos.forEach(aluno => {
-            const htmlRow = document.createElement('tr')
-            htmlRow.className = 'odd:bg-white even:bg-gray-100';
-            htmlRow.innerHTML = `<td class="px-6 py-3 text-left">${aluno.nome}</td>`
-            // console.log(Object.keys(aluno.notas)) - atenção para o uso de Object.keys, que retorna um array com as chaves do objeto
-            const htmlRowMaterias = this.materias.map(materia => `<td>${aluno.media[materia]}</td>`).join('')
-            htmlRow.innerHTML += htmlRowMaterias
-            this.tableBody.appendChild(htmlRow)
-        })
+        htmlHeader.innerHTML += htmlHeaderMaterias;
+        this.tableHeader.appendChild(htmlHeader);
     }
+
+    renderTbody(alunos) {
+        this.tableBody.innerHTML = '';
+    
+        alunos.forEach(aluno => {
+            const htmlRow = document.createElement('tr');
+            htmlRow.className = 'odd:bg-white even:bg-gray-100';
+    
+            let htmlMedia = `<td class="px-6 py-3 text-left">${aluno.nome}</td>`;
+            const btn = `<a href="#" data-nota-aluno="${aluno.nome}" class="text-gray-500 hover:text-blue-500 hover:border-b hover:border-dashed hover:border-blue-500">Incluir nota</a>`;
+    
+            const notas = aluno.nota || {};
+            let encontrado = false;
+    
+            this.materias.forEach(materia => {
+                if (materia in aluno.notas) {
+                    encontrado = true;
+                }
+            });
+    
+            if (encontrado) {
+                this.materias.forEach(materia => {
+                    htmlMedia += `<td class="px-6 py-3 text-left">
+                        ${
+                            aluno.media?.[materia] !== undefined
+                            ?  `<a href="#" data-nota-aluno="${aluno.media[materia]}" class="position-relative text-gray-500 hover:text-blue-500 hover:border-b hover:border-dashed hover:border-blue-500">
+                                ${aluno.media[materia]}
+
+                                </a>`
+                            : btn
+                        }
+                        <div class="position-absolute top-0 left-0 w-full h-full bg-gray-300 bg-opacity-50 rounded-md" style="display: none;">
+                            <input type="text"
+                                class="w-full h-full bg-gray-300 text-white border-none outline-none p-2 rounded-md"
+                                value="${aluno.media[materia]}"
+                                placeholder="Digite a nota do aluno"
+                            />
+                        </div>
+                    </td>`;
+                });
+            } else {
+                htmlMedia += `<td colspan="${this.materias.length}" class="px-6 py-3 text-left">${btn}</td>`;
+            }
+    
+            htmlRow.innerHTML = htmlMedia;
+            this.tableBody.appendChild(htmlRow);
+        });
+    }
+
 }
